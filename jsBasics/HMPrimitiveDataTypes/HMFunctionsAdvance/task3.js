eab927db6a9ff2203b25c7f675aca99fbc03a794
MichaelN98 <122857193+MichaelN98@users.noreply.github.com>
2026-01-16 09:43:37 +0200

    function advanced tasks by hm

---
+++ jsBasics/HMPrimitiveDataTypes/HMFunctionsAdvance/task3.js
@@ -0,0 +1,38 @@
+function divide(numerator, denominator) {
+  if (typeof numerator !== "number" || typeof denominator !== "number") {
+    throw new Error("Arguments must be numbers");
+  }
+
+  if (denominator === 0) {
+    throw new Error("Denominator cannot be 0");
+  }
+
+  return numerator / denominator;
+}
+
+// 1) ok
+try {
+  console.log(divide(10, 2));
+} catch (e) {
+  console.log("Error:", e.message);
+} finally {
+  console.log("Робота завершена");
+}
+
+// 2) denominator = 0
+try {
+  console.log(divide(10, 0));
+} catch (e) {
+  console.log("Error:", e.message);
+} finally {
+  console.log("Робота завершена");
+}
+
+// 3) not a number
+try {
+  console.log(divide("10", 2));
+} catch (e) {
+  console.log("Error:", e.message);
+} finally {
+  console.log("Робота завершена");
+}

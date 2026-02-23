3e0115e3aaf9c32cc81251975a6cc7ece8456b18
MichaelN98 <122857193+MichaelN98@users.noreply.github.com>
2026-02-23 23:21:53 +0200

    homework by OOP in Js

---
+++ jsBasics/HMPrimitiveDataTypes/HMOOP/EBook.js
@@ -0,0 +1,26 @@
+import Book from "./Book.js";
+
+export default class EBook extends Book {
+  constructor(title, author, year, format) {
+    super(title, author, year);
+    this.format = format;
+  }
+
+  printInfo() {
+    console.log(
+      this.title +
+        " - " +
+        this.author +
+        " (" +
+        this.year +
+        ") [" +
+        this.format +
+        "]"
+    );
+  }
+
+  //создать EBook из Book
+  static fromBook(book, format) {
+    return new EBook(book.title, book.author, book.year, format);
+  }
+}
